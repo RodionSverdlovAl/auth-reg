1d45d7a35a34a79661fe34166ec151526daf4fca
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-09-04 00:09:55 +0300

    add html and css

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <form>
+        <label>login</label>
+        <input name="login" type="text" placeholder="enter your login">
+        <label>password</label>
+        <input name="password" type="password" placeholder="enter your password">
+        <button>enter</button>
+        <a href="registration.php">registration</a>
+    </form>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<form method="post" action="" enctype="multipart/form-data">
+    <label>full name</label>
+    <input name="full_name" type="text" placeholder="enter your full name">
+    <label>login</label>
+    <input name="login" type="text" placeholder="enter your login">
+    <label>email</label>
+    <input name="email" type="text" placeholder="enter your email">
+    <label>profile image</label>
+    <input type="file">
+    <label>password</label>
+    <input name="password" type="password" placeholder="enter your password">
+    <label>repeat password</label>
+    <input name="password" type="password" placeholder="repeat password">
+    <button>registration</button>
+    <a href="index.php">authentication</a>
+</form>
+</body>
+</html>
