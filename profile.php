3e429e8f5234d2c5ff88b4d4bb0b144cabaf26d1
RodionSverdlovAl <rsverdlovalexandrovich@gmail.com>
2022-09-05 18:46:09 +0300

    add logout and fix reg and index

--- profile.php
+++ profile.php
@@ -1,17 +1,22 @@
 <?php
 session_start();
+if(!$_SESSION['user']){
+    header('Location: index.php');
+}
 ?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title><?=  $_SESSION['user']['login']; ?>Профиль</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
     <form>
+        <a href="vendor/logout.php">Exit</a>
         <h2><?=$_SESSION['user']['full_name']?></h2>
         <h3><?=$_SESSION['user']['email']?></h3>
-        <img src="<?=$_SESSION['user']['avatar']?>" width="500px">
+        <img src="<?=$_SESSION['user']['avatar']?>" width="300px">
 
     </form>
 </body>
--- registration.php
+++ registration.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+if($_SESSION['user']){
+    header('Location: ../profile.php');
+}
+
 ?>
 <!doctype html>
 <html lang="en">
